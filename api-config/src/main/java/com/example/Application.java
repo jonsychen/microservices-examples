af470d0a4aea1756a245c025cd9d151f4c693671
jonsy <work@jonsy.me>
2017-05-26 15:50:46 +0800

    修改包结构

---
+++ api-config/src/main/java/com/example/Application.java
@@ -0,0 +1,24 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author Jonsy
+ *
+ */
+@Configuration 
+@SpringBootApplication
+@EnableConfigServer
+@EnableEurekaClient
+@EnableAutoConfiguration  
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+}
