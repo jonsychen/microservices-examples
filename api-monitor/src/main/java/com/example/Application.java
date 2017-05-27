65cfe04f5ca82c714d6d674ff3e020679ae90174
jonsy <work@jonsy.me>
2017-05-27 11:27:28 +0800

    修改配置信息

--- api-monitor/src/main/java/com/example/Application.java
+++ api-monitor/src/main/java/com/example/Application.java
@@ -1,13 +1,10 @@
 package com.example;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.netflix.turbine.EnableTurbine;
-import org.springframework.context.annotation.Configuration;
-import de.codecentric.boot.admin.config.EnableAdminServer;
 
 /**
  * @author Jonsy
@@ -17,9 +14,7 @@ import de.codecentric.boot.admin.config.EnableAdminServer;
 @EnableHystrixDashboard
 @EnableTurbine
 @EnableEurekaClient
-@EnableAdminServer
-@EnableAutoConfiguration
-@Configuration
+//@EnableAdminServer
 public class Application {
 
 	public static void main(String[] args) {
